84159780b317fbdb672ceaa4f483924e747e7673
Jeff Pak <jeffrey.pak@emc.com>
2016-06-24 14:08:40 -0700

    custom help and translate tags
    Signed-off-by: Corbin Halliwill <challiwill@pivotal.io>

--- main.go
+++ main.go
@@ -4,15 +4,31 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"reflect"
 
 	"github.com/jessevdk/go-flags"
+	"github.com/nicksnyder/go-i18n/i18n"
 )
 
 func main() {
 
-	parser := flags.NewParser(&cli, flags.None)
+	parser := flags.NewParser(&cli, flags.HelpFlag)
 	_, err := parser.Parse()
-	if err != nil {
+	if flagsErr, ok := err.(*flags.Error); ok && flagsErr.Type == flags.ErrHelp {
+		fmt.Println("CUSTOM HELP MESSAGE")
+		field, found := reflect.TypeOf(cli).FieldByName("AppShow")
+		if !found {
+			fmt.Println("FIELD NOT FOUND")
+			os.Exit(1)
+		}
+		i18n.LoadTranslationFile("translations/fr-fr.all.json")
+		t, err := i18n.Tfunc("fr-FR")
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Println(t(field.Tag.Get("description")))
+	} else if err != nil {
 		fmt.Println("ERROR: ", err.Error())
 		os.Exit(1)
 	}
@@ -23,10 +39,13 @@ func main() {
 var cli CFCLI
 
 type CFCLI struct {
-	AppShow AppCmd `command:"app-show"`
+	AppShow AppCmd `command:"app-show" description:"show the app details"`
+	Help    HelpCmd
 }
 
 type AppCmd struct{}
+
+type HelpCmd struct{}
 
 func (cmd AppCmd) Execute(args []string) error {
 	if len(args) != 1 {
